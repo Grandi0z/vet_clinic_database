3aa893647b6ae00d644f31877107e7aa781b730d
Grandi0z <josephlkhausi@gmail.com>
2023-09-05 19:30:20 +0200

    Uddate data and added database ERD

--- schema.sql
+++ schema.sql
@@ -58,3 +58,10 @@ ON DELETE SET NULL;
 ALTER TABLE specializations ADD CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets (id)
 ON UPDATE CASCADE
 ON DELETE SET NULL;
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX idx_animal_id ON visits(animal_id);
+CREATE INDEX idx_vet_id ON visits(vet_id);
+CREATE INDEX idx_email ON owners(email);
+CLUSTER visits USING idx_animal_id;
